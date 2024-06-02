be58d3db15c6c4dd12b7f839be21ebd29ec5b84d
Devendra <singh2001devendra@gmail.com>
2024-06-02 17:40:36 +0530

    2nd part completed - ui design

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,35 @@
+// import { BiLogOut } from "react-icons/bi";
+// import useLogout from "../../hooks/useLogout";
+
+// const LogoutButton = () => {
+// 	const { loading, logout } = useLogout();
+
+// 	return (
+// 		<div className='mt-auto'>
+// 			{!loading ? (
+// 				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
+// 			) : (
+// 				<span className='loading loading-spinner'></span>
+// 			)}
+// 		</div>
+// 	);
+// };
+// export default LogoutButton;
+
+
+import { BiLogOut } from 'react-icons/bi';
+
+function LogoutButton() {
+ 
+
+  return (
+    <div className='mt-auto'>
+      <BiLogOut 
+        className='w-6 h-6 text-white cursor-pointer' 
+        
+      />
+    </div>
+  );
+}
+
+export default LogoutButton;
