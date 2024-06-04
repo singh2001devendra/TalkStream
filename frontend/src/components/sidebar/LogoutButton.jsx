35f5cf56f4ddcce991e090e70317afa4fd452f02
Devendra <singh2001devendra@gmail.com>
2024-06-04 22:52:21 +0530

    4th part completed- socket

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,35 +1,20 @@
-// import { BiLogOut } from "react-icons/bi";
-// import useLogout from "../../hooks/useLogout";
+import { BiLogOut } from "react-icons/bi";
+import useLogout from "../../hooks/useLogout";
 
-// const LogoutButton = () => {
-// 	const { loading, logout } = useLogout();
+const LogoutButton = () => {
+	const { loading, logout } = useLogout();
 
-// 	return (
-// 		<div className='mt-auto'>
-// 			{!loading ? (
-// 				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
-// 			) : (
-// 				<span className='loading loading-spinner'></span>
-// 			)}
-// 		</div>
-// 	);
-// };
-// export default LogoutButton;
-
-
-import { BiLogOut } from 'react-icons/bi';
-
-function LogoutButton() {
- 
-
-  return (
-    <div className='mt-auto'>
-      <BiLogOut 
-        className='w-6 h-6 text-white cursor-pointer' 
-        
-      />
-    </div>
-  );
-}
-
+	return (
+		<div className='mt-auto'>
+			{!loading ? (
+				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
+			) : (
+				<span className='loading loading-spinner'></span>
+			)}
+		</div>
+	);
+};
 export default LogoutButton;
+
+
+
