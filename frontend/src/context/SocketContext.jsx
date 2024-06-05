57bc2f90b09b830ce867590189024d01fc04055c
Devendra singh thakur <129576872+singh2001devendra@users.noreply.github.com>
2024-06-05 13:25:00 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chat-app-8n0j.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
